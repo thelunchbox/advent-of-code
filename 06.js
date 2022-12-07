0c5da544943910f83ec967b19cd8060b56765be3
Alex Merkert <alex.merkert@gmail.com>
2022-12-06 20:56:46 -0500

    oh man day 6 started out rough but now i'm happy

---
+++ 06.js
@@ -0,0 +1,48 @@
+const { findKey, sum } = require('./helpers.js');
+
+module.exports = (input) => {
+
+  const buffer = input[0].split(''); // it's just one line so it's all here
+  
+  const findUniqueSequence = (length) => {
+    let position = length - 1;
+    let check = buffer.slice(0, position);
+
+    const advanceCheck = (steps) => {
+      check.splice(0, steps);
+      check = [...check, ...buffer.slice(position, position + steps)];
+      position += steps;
+    }
+
+    const clearDuplicatesInCheck = () => {
+      for (let index = check.length - 2; index >= 0; index -= 1) {
+        if (check.slice(index + 1).includes(check[index])) {
+          advanceCheck(index + 1);
+          return true;
+        }
+      }
+    };
+
+    while (position < buffer.length) {
+      while(clearDuplicatesInCheck()) {};
+      const index = check.indexOf(buffer[position]);
+      // console.log('>> ', position, check.join(''), buffer[position], buffer.slice(position + 1, position + 10).join('') + '...', buffer.slice(position, position + index + 1).join(''));
+      if (index < 0) {
+        // we're done!
+        position += 1;
+        break;
+      }
+      advanceCheck(index + 1);
+    }
+
+    return position;
+  };
+
+  const part1 = findUniqueSequence(4);
+  const part2 = findUniqueSequence(14);
+
+  return {
+    part1,
+    part2,
+  };
+}
